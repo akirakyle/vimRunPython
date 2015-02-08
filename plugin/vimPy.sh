d20d26114cf9488e59b526b0bc00e87eeded0b83
akyle9 <akyle@andrew.cmu.edu>
2015-02-07 19:10:39 -0500

    testing

--- plugin/vimPy.sh
+++ plugin/vimPy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 FILEPATH=$1
+echo "$FILEPATH" >> ~/pyout
 #gstdbuf -o0 -e0 python $FILEPATH -u > pyout 2>&1 &
-gstdbuf -o0 -e0 python $FILEPATH -u > ~/pyout 2>&1
+gstdbuf -o0 -e0 python $FILEPATH -u >> ~/pyout 2>&1
 echo "~~finished running~~" >> ~/pyout
